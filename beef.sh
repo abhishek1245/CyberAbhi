e597ff61a11266d785af9dc6a910448c2cade578
abhishek1245 <47146843+abhishek1245@users.noreply.github.com>
2019-09-29 22:13:27 +0530

    Beef-xss

---
+++ beef.sh
@@ -0,0 +1,21 @@
+#!/data/data/com.termux/files/usr/bin/sh
+# Coded by abhishek chauhan
+echo "\..By {AbhishekChauhan}.../"| lolcat
+apt-get update -y
+apt-get upgrade -y
+pkg install nano -y
+pkg install git -y
+pkg install perl -y
+pkg install ruby -y
+pkg install nodejs -y 
+git clone https://github.com/beefproject/beef
+cd beef
+chmod +x *
+pkg upgrade 
+gem bundle install 
+bundle install 
+
+echo " "
+echo "###### Now use the tool type ./beef ######" | lolcat
+
+
