1a75ea6ac1e43d1a16c45599f34b42d69c4b0e36
abhishek1245 <47146843+abhishek1245@users.noreply.github.com>
2020-06-12 13:20:52 +0530

    Update beef.sh

--- beef.sh
+++ beef.sh
@@ -1,17 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/sh
 # Coded by abhishek chauhan
+apt install lolcat 
 echo "\..By {AbhishekChauhan}.../"| lolcat
 apt-get update -y
 apt-get upgrade -y
-pkg install nano -y
-pkg install git -y
-pkg install perl -y
-pkg install ruby -y
-pkg install nodejs -y 
+apt install nano -y
+apt install git -y
+apt install perl -y
+apt install ruby -y
+apt install nodejs -y 
 git clone https://github.com/beefproject/beef
 cd beef
 chmod +x *
-pkg upgrade 
+apt upgrade -y 
 gem bundle install 
 bundle install 
 
