34ba92a15d90d1a3132e0bda68188f483b257f5c
abhishek1245 <47146843+abhishek1245@users.noreply.github.com>
2020-06-12 13:27:06 +0530

    Update beef.sh

--- beef.sh
+++ beef.sh
@@ -13,8 +13,22 @@ git clone https://github.com/beefproject/beef
 cd beef
 chmod +x *
 apt upgrade -y 
-gem bundle install 
-bundle install 
+#install ruby 
+# Update rubygems-update
+if [ "$(gem list -i rubygems-update 2>/dev/null)" = "false" ]; then
+	gem install --no-document --verbose rubygems-update
+fi
+
+# update rubygems
+update_rubygems
+
+# Install bundler
+gem install --no-document --verbose bundler:1.17.3
+
+# Installing all gems 
+bundle config build.nokogiri --use-system-libraries
+bundle install -j3
+echo "Gems installed"
 
 echo " "
 echo "###### Now use the tool type ./beef ######" | lolcat
